f3dced83f2a9285eb6e370a49cc0166e33f05499
Alakananda <alakananda1294@gmail.com>
2018-08-25 16:30:49 +0530

    added constant class and change in App.java

--- src/main/java/com/nt/Git_demo/App.java
+++ src/main/java/com/nt/Git_demo/App.java
@@ -1,13 +1,9 @@
 package com.nt.Git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public void addData(String data){
-		System.out.println("data" +data);
-		if(data.equals("alaka")){
+		//System.out.println("data" +data);
+		if(data.equals(Constant.DATA_TYPE)){
 			System.out.println("welcome");
 		}
 	}
---
+++ src/main/java/com/nt/Git_demo/Constant.java
@@ -0,0 +1,5 @@
+package com.nt.Git_demo;
+
+public class Constant {
+	public static String DATA_TYPE="alaka";
+}
