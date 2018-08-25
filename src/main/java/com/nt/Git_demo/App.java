fa4a97506e046b75749c1603ec95dc88a048fa23
Alakananda <alakananda1294@gmail.com>
2018-08-25 15:45:38 +0530

    added if condition to check the data

--- src/main/java/com/nt/Git_demo/App.java
+++ src/main/java/com/nt/Git_demo/App.java
@@ -7,6 +7,9 @@ package com.nt.Git_demo;
 public class App {
 	public void addData(String data){
 		System.out.println("data" +data);
+		if(data.equals("alaka")){
+			System.out.println("welcome");
+		}
 	}
 
     public static void main( String[] args )
